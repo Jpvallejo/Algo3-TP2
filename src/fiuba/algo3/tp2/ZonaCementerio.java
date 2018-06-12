cc0dec605e8a5b3c2b45ffbcce498f0f7d93221c
Demian <dpoggio@cablevision.com.ar>
2018-06-12 14:20:12 -0300

    Cementerio - V2

--- src/fiuba/algo3/tp2/ZonaCementerio.java
+++ src/fiuba/algo3/tp2/ZonaCementerio.java
@@ -1,12 +1,28 @@
 package fiuba.algo3.tp2;
 
+import java.util.ArrayList;
+
 public class ZonaCementerio extends Zona {
+
+    // Esta bien hacer que casilleros
+    ArrayList<Carta> cartas;
+
+    public ZonaCementerio(){
+
+        this.cartas = new ArrayList<Carta>();
+    }
 
 
     public void enviarCarta(Carta carta) {
+        cartas.add(carta);
     }
 
     public Carta buscarCarta(Carta carta) {
         return carta;
     }
+
+
+    public boolean estaVacio(){
+        return cartas.isEmpty();
+    };
 }
--- test/fiuba/algo3/tp2/ZonaTest.java
+++ test/fiuba/algo3/tp2/ZonaTest.java
@@ -2,9 +2,29 @@ package fiuba.algo3.tp2;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class ZonaTest {
+
+    @Test
+    public void testCrearCementerioGeneraCementerioVacio() {
+
+        ZonaCementerio cementerio = new ZonaCementerio();
+        assertTrue(cementerio.estaVacio());
+
+    }
+
+    @Test
+    public void testCrearCementerioNoEstaVacioSiSeEnviaUnaCarta() {
+
+        Carta carta = new CartaMonstruo();
+        ZonaCementerio cementerio = new ZonaCementerio();
+
+        cementerio.enviarCarta(carta);
+        assertFalse(cementerio.estaVacio());
+
+    }
+
     @Test
     public void testBuscarCartaEnCementerioObtieneLaMismaCartaSiEstaSeEncuentraAhi() {
 
