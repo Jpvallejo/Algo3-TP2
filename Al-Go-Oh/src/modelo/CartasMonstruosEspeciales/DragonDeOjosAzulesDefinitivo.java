8f140e826c790df5e8d71f63137a6e34dcd1c285
Demian <dpoggio@cablevision.com.ar>
2018-07-03 00:09:17 -0300

    Alternativa a instanceof en sacrificio de dragones

--- Al-Go-Oh/src/modelo/CartasMonstruosEspeciales/DragonDeOjosAzulesDefinitivo.java
+++ Al-Go-Oh/src/modelo/CartasMonstruosEspeciales/DragonDeOjosAzulesDefinitivo.java
@@ -27,14 +27,30 @@ public class DragonDeOjosAzulesDefinitivo extends Monstruo {
 
     @Override
     public void requiereSacrificios(Monstruo sacrificio1, Monstruo sacrificio2, Monstruo sacrificio3){
-        if ((sacrificio1 instanceof DragonBlancoDeOjosAzules) && (sacrificio2 instanceof DragonBlancoDeOjosAzules) && (sacrificio3 instanceof DragonBlancoDeOjosAzules) ) {
+
+      /*  if ((sacrificio1 instanceof DragonBlancoDeOjosAzules) && (sacrificio2 instanceof DragonBlancoDeOjosAzules) && (sacrificio3 instanceof DragonBlancoDeOjosAzules) ) {
             sacrificio1.destruir();
             sacrificio2.destruir();
             sacrificio3.destruir();
         }
         else{
             throw new MonstruoDeFusionException();
+        }*/
+        // Buscar otra forma de sacrificar...
+        try {
+            sacrificioEspecial((DragonBlancoDeOjosAzules) sacrificio1, (DragonBlancoDeOjosAzules) sacrificio2, (DragonBlancoDeOjosAzules) sacrificio3);
         }
+        catch(Exception e){
+            throw new MonstruoDeFusionException();
+        }
+
+    }
+
+
+    public void sacrificioEspecial(DragonBlancoDeOjosAzules d1, DragonBlancoDeOjosAzules d2, DragonBlancoDeOjosAzules d3){
+        d1.destruir();
+        d2.destruir();
+        d3.destruir();
     }
 
 
